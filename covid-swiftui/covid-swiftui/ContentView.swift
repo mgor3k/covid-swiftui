24309313a08d110caf780979c27c2113df60fcfd
Maciej Górecki <maciekgorecki@icloud.com>
2020-10-06 18:37:22 +0200

    Created project

---
+++ covid-swiftui/covid-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  covid-swiftui
+//
+//  Created by Maciej Gorecki on 06/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ covid-swiftui/covid-swiftui/covid_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  covid_swiftuiApp.swift
+//  covid-swiftui
+//
+//  Created by Maciej Gorecki on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct covid_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
